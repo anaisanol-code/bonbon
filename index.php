f519124dd69368c369f880f477c91215a11b4092
Charmaine PANHUYZEN <charmaine.panhuyzen@etu.univ-guyane.fr>
2025-09-11 11:33:06 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Mon site</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+  <header>
+    <nav>
+      <a href="#">Home</a> |
+      <a href="#">About</a> |
+      <a href="#">Contact</a>
+    </nav>
+
+    <div class="img">
+      <img src="images/entete.webp" alt="Image d'en-tête">
+    </div>
+  </header>
+
+</body>
+</html>
